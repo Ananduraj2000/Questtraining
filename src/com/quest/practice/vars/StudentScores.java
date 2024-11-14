f5d8fffda39ee02554204e1a296d06556a2a3b5c
Anandu2000 <ananduraj42412@gmail.com>
2024-11-14 18:04:12 +0530

    updated student score

--- src/com/quest/practice/vars/StudentScores.java
+++ src/com/quest/practice/vars/StudentScores.java
@@ -3,7 +3,6 @@ package com.quest.practice.vars;
 public class StudentScores {
 
 
-
     public static void main(String[] args) {
         String[] names = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace"};
         int[] ages = {20, 21, 22, 23, 24, 25, 26};
@@ -49,7 +48,7 @@ public class StudentScores {
             } else {
                 average = 0;
             }
-            System.out.println("Average score of "+names[i]+" of age "+ ages[i]+" is "+ average);
+            System.out.println("Average score of " + names[i] + " of age " + ages[i] + " is " + average);
         }
     }
 
@@ -81,26 +80,61 @@ public class StudentScores {
         }
 
         if (highestIndex != -1) {
-            System.out.printf("Highest Scorer is "+names[highestIndex]+" of age "+ages[highestIndex]+
-                    " has an average score of "+highestAverage);
+            System.out.printf("Highest Scorer is " + names[highestIndex] + " of age " + ages[highestIndex] +
+                    " has an average score of " + highestAverage);
+            System.out.println();
+            System.out.println("-------------------------------------------------");
         }
     }
 
     // Method to rank students based on average score
     public static void rankStudents(String[] names, int[] ages, int[][] scores) {
-        // Logic to rank students by average score
+        int[] sum=new int[names.length];
+        int[] avg=new int[names.length];
+
+        for(int i=0; i<names.length; i++){
+            for(int j=0; j<scores[i].length; j++) {
+                sum[i] += scores[i][j];
+            }
+            avg[i]=sum[i]/scores[i].length;
+
+        }
+        for (int i = 0; i < avg.length ; i++) {
+            for (int j =i+1; j < avg.length ; j++) {
+                if (avg[i] < avg[j]) {
+
+                    int temp = avg[i];
+                    avg[i] = avg[j];
+                    avg[j] = temp;
+
+                    String name=names[i];
+                    names[i]=names[j];
+                    names[j]=name;
+                }
+            }
+
+        }
+        System.out.println("Student ranking");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println((i + 1) + ". " + names[i] + " - Average Score: " + avg[i]);
+        }
     }
 
     // Method to detect missing data in the scores array
     public static void detectMissingData(String[] names, int[][] scores) {
+        System.out.println();
         System.out.println("Missing Scores:");
         for (int i = 0; i < scores.length; i++) {
             boolean hasMissing = false;
             System.out.println(names[i] + ":");
             for (int j = 0; j < scores[i].length; j++) {
                 if (scores[i][j] == -1) {
-
+                    System.out.println("Subject " + (j + 1) + " ");
+                    hasMissing = true;
                 }
+            }
+            if (!hasMissing) {
+                System.out.println("No missing score");
             }
         }
     }
